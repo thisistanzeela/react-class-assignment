2ccbf73595c0f3fda1ea2b52cb8c51348ee8cb41
thisistanzeela <tanzeelamansoor99@gmail.com>
2023-06-01 20:45:31 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1533,49 +1533,111 @@
 import React, { useState, useEffect } from 'react';
 import "./App.css";
 
-const  App=()=> {
+// const  App=()=> {
+//   const [users, setUsers] = useState([]);
+//   const [selectedUser, setSelectedUser] = useState(null);
+  
+//   useEffect(() => {
+//     fetch('https://api.github.com/users')
+//       .then((response) => response.json())
+//       .then((data) => setUsers(data))
+//       .catch((error) => console.log(error));
+//   }, []);
+  
+//   const handleClick = (username) => {
+//     fetch(`https://api.github.com/users/${username}`)
+//       .then((response) => response.json())
+//       .then((data) => setSelectedUser(data))
+//       .catch((error) => console.log(error));
+//   };
+  
+//   return (
+//     <div className='main'>
+//       <div className="grid-col-1">
+//         <h1>GitHub Users</h1>
+//         <ul >
+//           {users.map((user) => (
+//             <li key={user.id} onClick={() => handleClick(user.login)}>
+//               {user.login}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>        
+//       <div className="grid-col-2">
+//         {selectedUser && (
+//           <div className='user-details'>
+//             <h2>Username: {selectedUser.login}</h2>
+//             <p>Twitter Username: {selectedUser.twitter_username}</p>
+//             <p>Location: {selectedUser.location}</p>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+
+const App = () => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
-  
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    fetch('https://api.github.com/users')
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.log(error));
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://api.github.com/users');
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchUsers();
   }, []);
-  
-  const handleClick = (username) => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => response.json())
-      .then((data) => setSelectedUser(data))
-      .catch((error) => console.log(error));
+
+  const handleClick = async (username) => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch user details');
+      }
+      const data = await response.json();
+      setSelectedUser(data);
+    } catch (error) {
+      setError(error.message);
+    }
   };
-  
+
   return (
-    <div className='main'>
+    <div className="main">
       <div className="grid-col-1">
         <h1>GitHub Users</h1>
-        <ul >
+        {error && <p>Error: {error}</p>}
+        <ul>
           {users.map((user) => (
             <li key={user.id} onClick={() => handleClick(user.login)}>
               {user.login}
             </li>
           ))}
         </ul>
-      </div>        
+      </div>
       <div className="grid-col-2">
         {selectedUser && (
-          <div className='user-details'>
+          <div className="user-details">
             <h2>Username: {selectedUser.login}</h2>
-            <p>Twitter Username: {selectedUser.twitter_username}</p>
-            <p>Location: {selectedUser.location}</p>
+            <p>Twitter Username: {selectedUser.twitter_username || 'N/A'}</p>
+            <p>Location: {selectedUser.location || 'N/A'}</p>
           </div>
         )}
       </div>
     </div>
   );
-}
-
+};
 
 export { App};
 
